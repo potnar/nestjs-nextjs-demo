ed9c0126694e2bc8cbb036306b4f9a3a4a7a4ad8
Mikolaj <mikolaj@merope.pl>
2025-10-03 09:57:18 +0200

    delete filesystem from server

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { FilesystemModule } from './filesystem/filesystem.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { WeightModule } from './weight/weight.module';
 
 @Module({
-  imports: [UsersModule, FilesystemModule, PrismaModule, WeightModule],
+  imports: [UsersModule, PrismaModule, WeightModule],
   controllers: [AppController],
   providers: [AppService],
 })
